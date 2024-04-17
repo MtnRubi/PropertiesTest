6c8da48c8d605a89f4b7d3f89dd04351e1cab140
dkrause <dkrause@unixplanet.net>
2024-04-17 13:30:32 -0700

    Needed a @PostConstruct method, attempting to print in the constructor doesn't work as it needs to be fully realized prior to field injection.

--- src/main/java/net/unixplanet/randomcrap/propertiestest/PropTest.java
+++ src/main/java/net/unixplanet/randomcrap/propertiestest/PropTest.java
@@ -1,15 +1,22 @@
 package net.unixplanet.randomcrap.propertiestest;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PropTest {
 
-    @Value("${random.crap}")
+    //@Value("${random.crap}") <-- this is not working, see note in application.properties
+    //Also, if you have quotes around the value, you'll get quotes in the output
+    @Value("${rand.crap}")
     private String crap;
-
+    
     public PropTest() {
+    }
+
+    @PostConstruct
+    public void init() {
         System.out.println("Random value: " + crap);
     }
 }
